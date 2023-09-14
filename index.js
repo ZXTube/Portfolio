ac635df6a64d68610e583743ea56372f9fecbcb1
ZiyadCodes <82272898+ZXTube@users.noreply.github.com>
2023-09-14 21:14:18 +0100

    Added images and improved styling

--- index.js
+++ index.js
@@ -25,3 +25,37 @@ for (let i = 0; i < skills.length; i++) {
         skills[i].style.setProperty("--expanded-height", height.toString() + "px");
     }
 }
+const projectImagesHolder = document.getElementsByClassName("project-images-holder");
+
+for (i = 0; i < projectImagesHolder.length; i++) {
+    const images = projectImagesHolder[i].getElementsByTagName("div")[0].querySelectorAll("img");
+    let currentIndex = 0;
+
+    function showImage(index) {
+        images.forEach((img, i) => {
+            if (i === index) {
+                img.setAttribute("visible", "true");
+            } else {
+                img.setAttribute("visible", "false");
+            }
+        });
+    }
+
+    function nextImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        showImage(currentIndex);
+    }
+    function prevImage() {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        showImage(currentIndex);
+    }
+
+    const nextBtn = projectImagesHolder[i].getElementsByClassName("next-btn")[0];
+    const prevBtn = projectImagesHolder[i].getElementsByClassName("prev-btn")[0];
+    nextBtn.addEventListener("click", nextImage);
+    prevBtn.addEventListener("click", prevImage);
+
+    showImage(currentIndex);
+    const curProject = projectImagesHolder[i];
+    setTimeout(() => setInterval(() => { if (!curProject.matches(":hover")) nextImage() }, 3000), Math.floor(Math.random() * 2500));
+}
