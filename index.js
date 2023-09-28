35fc7df827620c332487f76fbf627770b291a371
ZiyadCodes <82272898+ZXTube@users.noreply.github.com>
2023-09-28 16:24:40 +0100

    Made projects selectable

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 let skills = document.getElementsByClassName("skill");
 let originalSkillBorderRadius = skills[0].style.borderRadius;
 for (let i = 0; i < skills.length; i++) {
-    let filter = skills[i].children[0];
+    const filter = skills[i].children[0];
     filter.onclick = () => {
-        let arrow = filter.children[1];
+        const arrow = filter.children[1];
         if (arrow.getAttribute("opened") == "true") {
             arrow.setAttribute("opened", "false");
             skills[i].setAttribute("expanded", "false");
@@ -15,20 +15,34 @@ for (let i = 0; i < skills.length; i++) {
     }
 
     requestAnimationFrame(setExpandedHeight);
-
+    
     function setExpandedHeight() {
+        const skillChildren = skills[i].children;
         let height = skills[i].offsetHeight;
-        let skillChildren = skills[i].children;
         for (let i = 1; i < skillChildren.length; i += 2) {
             height += skillChildren[i].offsetHeight;
         }
         skills[i].style.setProperty("--expanded-height", height.toString() + "px");
     }
 }
-const projectImagesHolder = document.getElementsByClassName("project-images-holder");
+const projectImagesHolders = document.getElementsByClassName("project-images-holder");
 
-for (i = 0; i < projectImagesHolder.length; i++) {
-    const images = projectImagesHolder[i].getElementsByTagName("div")[0].querySelectorAll("img");
+for (i = 0; i < projectImagesHolders.length; i++) {
+    const project = projectImagesHolders[i].parentNode
+    project.addEventListener('click', () => {
+        const selectedProjectHolder = document.getElementById('selected-project-holder');
+        selectedProjectHolder.setAttribute('visible', 'true');
+
+        const children = project.children;
+        for (let i = 0; i < children.length; i++) {
+            selectedProjectHolder.appendChild(children[i]);
+        }
+        const [_prevBtn, _nextBtn] = Array.from(selectedProjectHolder.getElementsByTagName('div')[0].children).slice(-2);
+        _prevBtn.style.left = '4.7vw';
+        _nextBtn.style.right = '4.7vw';
+    });
+
+    const images = projectImagesHolders[i].getElementsByTagName("div")[0].querySelectorAll("img");
     let currentIndex = 0;
 
     function showImage(index) {
@@ -50,12 +64,12 @@ for (i = 0; i < projectImagesHolder.length; i++) {
         showImage(currentIndex);
     }
 
-    const nextBtn = projectImagesHolder[i].getElementsByClassName("next-btn")[0];
-    const prevBtn = projectImagesHolder[i].getElementsByClassName("prev-btn")[0];
+    const nextBtn = projectImagesHolders[i].getElementsByClassName("next-btn")[0];
+    const prevBtn = projectImagesHolders[i].getElementsByClassName("prev-btn")[0];
     nextBtn.addEventListener("click", nextImage);
     prevBtn.addEventListener("click", prevImage);
 
     showImage(currentIndex);
-    const curProject = projectImagesHolder[i];
+    const curProject = projectImagesHolders[i];
     setTimeout(() => setInterval(() => { if (!curProject.matches(":hover")) nextImage() }, 3000), Math.floor(Math.random() * 2500));
 }
